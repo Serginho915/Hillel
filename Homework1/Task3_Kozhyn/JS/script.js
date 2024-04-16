d9a80cbe00f495365e4176f11766af00fbfd83fd
Serhii Kozhyn <serhiikozhyn@Noutbuk-Sergej.local>
2024-04-16 23:51:30 +0100

    remastered third task

--- Homework1/Task3_Kozhyn/JS/script.js
+++ Homework1/Task3_Kozhyn/JS/script.js
@@ -1,16 +1,8 @@
-debugger;
+
 let number = prompt("enter a five-digit number");
-switch ( !(typeof(+number)=== `number`)){
-
-    case true:
-        let res = '';
-        number.toString()
-        for (let i = 0; i < number.length; i++) {
-            res += number[i] + " ";
-        }  
-        alert(res);
-        break;
-    default:
-        alert(`Try again.`);
-}
-
+let res = '';
+ number.toString()
+     for (let i = 0; i < number.length; i++) {
+         res += number[i] + " ";
+     }  
+alert(res);
