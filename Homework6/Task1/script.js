8fb91c42f20e965d3dff124e57e47bde10ac17ab
Serhii Kozhyn <serginho915@gmail.com>
2024-05-05 16:27:53 +0100

    finished 6HM 1TSK

--- Homework6/Task1/script.js
+++ Homework6/Task1/script.js
@@ -1,20 +1,20 @@
-let ladder = {
-    step: 0,
-    up: function() {
-      this.step++;
-      return this;
+let ladder ={
+    step : 0,
+    up: function () { 
+        this.step++;
+        return this;
     },
-    down: function() {
-      if (this.step > 0) {
-        this.step--;
-      }
-      return this;
+    down: function () { 
+        if (this.step > 0) {
+            this.step--;
+        }
+        return this;
     },
-    showStep: function() {
-      console.log(this.step);
-      return this;
+    showStep: function () { 
+        console.log(this.step);
+        return this;
     }
-  };
-  
-  ladder.up().up().down().showStep(); 
-  
+};
+
+
+ladder.up().up().down().showStep()
