42b38c5113981d77a27a42c1d16b9b1f57849f9e
Serhii Kozhyn <serginho915@gmail.com>
2024-06-25 15:18:07 +0100

    Homework15task1

---
+++ Homework15/script.js
@@ -0,0 +1,33 @@
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+    }
+  }
+
+  withdraw(amount) {
+    if (amount > 0 && amount <= this.balance) {
+      this.balance -= amount;
+    }
+  }
+}
+
+const account1 = new BankAccount(1000);
+
+console.log(account1.getBalance()); // 1000
+
+account1.deposit(500);
+
+console.log(account1.getBalance()); // 1500
+
+account1.withdraw(200);
+
+console.log(account1.getBalance()); // 1300
