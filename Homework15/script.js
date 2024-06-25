e1808cc6aec54fd07d1b41072982408dda23dabb
Serhii Kozhyn <serginho915@gmail.com>
2024-06-25 15:20:51 +0100

    Homework_15task_1

--- Homework15/script.js
+++ Homework15/script.js
@@ -1,33 +1,47 @@
 class BankAccount {
-  constructor(balance) {
-    this.balance = balance;
+  constructor(initialBalance) {
+    this._balance = initialBalance;
   }
 
-  getBalance() {
-    return this.balance;
+  get balance() {
+    return this._balance;
+  }
+
+  set balance(amount) {
+    if (amount >= 0) {
+      this._balance = amount;
+    } else {
+      throw new Error("Balance cannot be negative");
+    }
   }
 
   deposit(amount) {
     if (amount > 0) {
-      this.balance += amount;
+      this.balance = this._balance + amount;
+    } else {
+      throw new Error("Deposit amount must be positive");
     }
   }
 
   withdraw(amount) {
-    if (amount > 0 && amount <= this.balance) {
-      this.balance -= amount;
+    if (amount > 0 && amount <= this._balance) {
+      this.balance = this._balance - amount;
+    } else if (amount <= 0) {
+      throw new Error("Withdrawal amount must be positive");
+    } else {
+      throw new Error("Insufficient funds");
     }
   }
 }
 
 const account1 = new BankAccount(1000);
 
-console.log(account1.getBalance()); // 1000
+console.log(account1.balance); // 1000
 
 account1.deposit(500);
 
-console.log(account1.getBalance()); // 1500
+console.log(account1.balance); // 1500
 
 account1.withdraw(200);
 
-console.log(account1.getBalance()); // 1300
+console.log(account1.balance); // 1300
